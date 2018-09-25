3eb0c67c3c1ec1e4e927de2b9acca0b41325b2b4
Troels Ugilt Jensen <tujensen@gmail.com>
2018-09-25 11:21:37 +0200

    DOKKWAY-252: Fixed first version of cleanup function

--- Command/CleanupCommand.php
+++ Command/CleanupCommand.php
@@ -16,6 +16,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Output\OutputInterface;
+use Os2Display\MediaBundle\Entity\Media;
 
 /**
  * Class CleanupCommand
@@ -39,6 +40,47 @@ class CleanupCommand extends ContainerAwareCommand {
   }
 
   /**
+   * Delete an array of entities.
+   *
+   * @param $entityList
+   * @param $cleanupService
+   * @param $output
+   * @param bool $dryRun
+   *
+   * @return int Number of deleted items.
+   */
+  private function deleteEntities($entityList, $cleanupService, $output, $dryRun = true) {
+    $entitiesDeleted = 0;
+
+    foreach ($entityList as $entity) {
+      $deleteSuccess = true;
+
+      $name = null;
+
+      if (get_class($entity) == Media::class) {
+        $name = $entity->getName();
+      }
+      else {
+        $name = $entity->getTitle();
+      }
+
+      if (!$dryRun) {
+        $deleteSuccess = $cleanupService->deleteEntity($entity);
+        $entitiesDeleted++;
+      }
+
+      if ($deleteSuccess) {
+        $output->writeln('Deleted "' . $name .'" (id: ' . $entity->getId() . ')');
+      }
+      else {
+        $output->writeln('Error deleting "' . $name .'" (id: ' . $entity->getId() . ')');
+      }
+    }
+
+    return $entitiesDeleted;
+  }
+
+  /**
    * Executes the command
    *
    * @param InputInterface $input
@@ -48,8 +90,18 @@ class CleanupCommand extends ContainerAwareCommand {
   protected function execute(InputInterface $input, OutputInterface $output) {
     $io = new SymfonyStyle($input, $output);
 
+    $dryRun = false;
+
+    if ($input->hasOption('dry-run')) {
+      $dryRun = $input->getOption('dry-run');
+      $output->writeln('Dry-run: ' . ($dryRun ? 'true' : 'false'));
+    }
+
     $deleteBeforeDate = $io->confirm('Set threshold for deletion?', false);
     $timestampThreshold = null;
+    $numberOfDeletedMedia = 0;
+    $numberOfDeletedSlides = 0;
+    $numberOfDeletedChannels = 0;
 
     if ($deleteBeforeDate) {
       $oneYearAgo = strtotime("-1 year", time());
@@ -62,16 +114,67 @@ class CleanupCommand extends ContainerAwareCommand {
       $output->writeln('Selected date: ' . $selectedDate . " (" . $timestampThreshold . ")");
     }
 
-    // Get lists of content that should be deleted.
     $cleanupService = $this->getContainer()->get('os2display.core.cleanup_service');
 
+    // Deleting Media
     $mediaList = $cleanupService->findMediaToDelete($timestampThreshold);
+    if (count($mediaList) > 0) {
+      $confirm = $io->confirm('This will delete ' . count($mediaList) . ' media. Do you wish to continue?', FALSE);
+      if (!$confirm) {
+        $output->writeln('Cleanup cancelled!');
+        return;
+      }
+      $output->writeln('Deleting media...');
+      $numberOfDeletedMedia = $this->deleteEntities($mediaList, $cleanupService, $output, $dryRun);
+      $output->writeln('');
+    }
+    else {
+      $output->writeln('No media found for deletion.');
+    }
 
-    $confirm = $io->confirm('This will delete data. Do you wish to continue?', false);
+    // Deleting Slides
+    $slideList = $cleanupService->findSlidesToDelete($timestampThreshold);
+    if (count($slideList) > 0) {
+      $confirm = $io->confirm('This will delete ' . count($slideList) . ' slides. Do you wish to continue?', false);
+      if (!$confirm) {
+        $output->writeln('Cleanup cancelled!');
+        return;
+      }
+      $output->writeln('Deleting slides...');
+      $numberOfDeletedSlides = $this->deleteEntities($slideList, $cleanupService, $output, $dryRun);
+      $output->writeln('');
+    }
+    else {
+      $output->writeln('No slides found for deletion.');
+    }
 
 
+    // Deleting Channels
+    $channelList = $cleanupService->findChannelsToDelete($timestampThreshold);
+    if (count($channelList) > 0) {
+      $confirm = $io->confirm('This will delete ' . count($channelList) . ' channels. Do you wish to continue?', false);
+      if (!$confirm) {
+        $output->writeln('Cleanup cancelled!');
+        return;
+      }
+      $output->writeln('Deleting channels...');
+      $numberOfDeletedChannels = $this->deleteEntities($channelList, $cleanupService, $output, $dryRun);
+      $output->writeln('');
+    }
+    else {
+      $output->writeln('No channels found for deletion.');
+    }
 
     $output->writeln('');
-    $output->writeln('Cleanp done.');
+    $output->writeln('### Summery ###');
+    if ($dryRun) {
+      $output->writeln('Dry-run enabled. No entities deleted.');
+    }
+    $output->writeln('Media deleted: ' . $numberOfDeletedMedia);
+    $output->writeln('Slides deleted: ' . $numberOfDeletedSlides);
+    $output->writeln('Channels deleted: ' . $numberOfDeletedChannels);
+    $output->writeln('### Summery ###');
+    $output->writeln('');
+    $output->writeln('Cleanup done.');
   }
 }
--- Services/CleanupService.php
+++ Services/CleanupService.php
@@ -4,6 +4,8 @@ namespace Os2Display\CoreBundle\Services;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Os2Display\MediaBundle\Entity\Media;
+use Os2Display\CoreBundle\Entity\Slide;
+use Os2Display\CoreBundle\Entity\Channel;
 
 /**
  * Class CleanupService
@@ -23,14 +25,75 @@ class CleanupService {
   }
 
   /**
-   * Find unused media and
+   * Find unused media and media with updatedAt lower than threshold timestamp.
    *
    * @param int $threshold Optional threshold. If set all items with modification
    *                       date before threshold will be included.
+   * @return mixed Media.
    */
   public function findMediaToDelete($threshold = null) {
-    $unusedMedia = $this->mediaRepository->findBy(['mediaOrder' => null]);
+    $qb = $this->entityManager->createQueryBuilder();
 
-    $p = 1;
+    $query = $qb->select('m')
+      ->from(Media::class, 'm')
+      ->where('m.mediaOrders is empty');
+
+    if (!is_null($threshold)) {
+      $query->orWhere('m.updatedAt < :threshold')
+      ->setParameter('threshold', \DateTime::createFromFormat('U', $threshold));
+    }
+
+    return $query->getQuery()->getResult();
+  }
+
+  /**
+   * Find unused slides and slides with modifiedAt lower than threshold timestamp.
+   *
+   * @param int $threshold Optional threshold. If set all items with modification
+   *                       date before threshold will be included.
+   * @return mixed Slide.
+   */
+  public function findSlidesToDelete($threshold = null) {
+    $qb = $this->entityManager->createQueryBuilder();
+
+    $query = $qb->select('s')
+      ->from(Slide::class, 's')
+      ->where('s.channelSlideOrders is empty');
+
+    if (!is_null($threshold)) {
+      $query->orWhere('s.modifiedAt < :threshold')
+        ->setParameter('threshold', $threshold);
+    }
+
+    return $query->getQuery()->getResult();
+  }
+
+  /**
+   * Find unused channels and channels with modifiedAt lower than threshold timestamp.
+   *
+   * @param int $threshold Optional threshold. If set all items with modification
+   *                       date before threshold will be included.
+   * @return mixed Channel.
+   */
+  public function findChannelsToDelete($threshold = null) {
+    $qb = $this->entityManager->createQueryBuilder();
+
+    $query = $qb->select('c')
+      ->from(Channel::class, 'c')
+      ->where('c.channelScreenRegions is empty');
+
+    if (!is_null($threshold)) {
+      $query->orWhere('c.modifiedAt < :threshold')
+        ->setParameter('threshold', $threshold);
+    }
+
+    return $query->getQuery()->getResult();
+  }
+
+  public function deleteEntity($entity) {
+    $this->entityManager->remove($entity);
+    $this->entityManager->flush();
+
+    return true;
   }
 }
